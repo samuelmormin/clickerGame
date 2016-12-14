2993de73c45973d92ae9e6e373a7a19137dd877e
CharlyLeMatou <charl.y21@hotmail.fr>
2016-12-14 16:07:40 +0100

    debug percentage + animationClickMouse

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,17 +1,33 @@
 var planetEarth          = document.querySelector(".planet");
-var activePlanet         = document.querySelectorAll(".planet div");
-var currentPercentage    = parseInt(parseFloat(clicker.global_var.purification_current/clicker.global_var.purification*100))
-console.log(activePlanet);
+var mouseDiv             = document.querySelector("div.mouseClickEffect");
+var mouse                = { x : 0, y : 0 };
+var mouseDivPosition     = { x : 0, y : 0 };
 
-planetEarth.addEventListener('click', function(){
+
+planetEarth.addEventListener('click', function()
+{
     planetEarth.classList.add('clickEffect1')
 });
 
-
-planetEarth.addEventListener('animationend', function(){
+planetEarth.addEventListener('animationend', function()
+{
     planetEarth.classList.remove('clickEffect1');
 });
 
-currentPercentage.addEventListener('', function(){
+document.addEventListener('mousemove', function(event)
+{
+    mouse.x                 = event.clientX;
+    mouse.y                 = event.clientY;
+    mouseDivPosition.x      = mouse.x + 10;
+    mouseDivPosition.y      = mouse.y - 10;
+    mouseDiv.style.left     =  mouseDivPosition.x + "px";
+    mouseDiv.style.top      =  mouseDivPosition.y + "px";
+});
 
+
+document.addEventListener('click', function()
+{
+    var element = document.createElement("P");
+    var p = document.createTextNode("+1");
+    
 });
